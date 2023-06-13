d04f5744cb7f55c65dbcca7e43700bfd1b840368
leolopes2001 <leomartins2001@gmail.com>
2023-06-13 17:12:47 -0300

    Add carrossel

--- index.js
+++ index.js
@@ -15,10 +15,45 @@ document.querySelectorAll(".day_container_section_6 > div:not(.tab)").forEach((e
 
         const containerDayId = containerDay.id
         const hideContainerDayId = containerDayId === "day_1" ? 2: 1;
-
-
         document.querySelector(`#schedule_${containerDayId}`).style.display = "block"
         document.querySelector(`#schedule_day_${hideContainerDayId}`).style.display = "none"
-
     })
 })
+
+$(function () {
+    if ($('.owl-2').length > 0) {
+      $('.owl-2').owlCarousel({
+        autoplayTimeout: 2500,
+        center: false,
+        items: 1,
+        loop: true,
+        stagePadding: 0,
+        margin: 20,
+        smartSpeed: 1000,
+        autoplay: true,
+        nav: true,
+        dots: true,
+        pauseOnHover: false,
+        responsive: {
+          600: {
+            margin: 20,
+            nav: true,
+            items: 2
+          },
+          1000: {
+            margin: 20,
+            stagePadding: 0,
+            nav: true,
+            items: 3
+          },
+          1200: {
+            margin: 20,
+            stagePadding: 0,
+            nav: true,
+            items: 4
+          }
+        }
+      });
+    }
+  
+  })
